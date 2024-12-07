96737a461dea8bb1918d0f17ea6a541ae4fd32b4
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-07 14:47:28 -0300

    Edit Containers

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -1,4 +1,4 @@
 DROP USER IF EXISTS 'rails_user'@'%';
 CREATE USER 'rails_user'@'%' IDENTIFIED BY '05092004';
-GRANT ALL PRIVILEGES ON g_prod_development.* TO 'rails_user'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'rails_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
