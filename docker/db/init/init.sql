f0b0a8de587408e23eef296a3a59d8560e142f3b
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-07 14:39:35 -0300

    Edit: init.sql-pwd

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -1,4 +1,4 @@
 DROP USER IF EXISTS 'rails_user'@'%';
-CREATE USER 'rails_user'@'%' IDENTIFIED BY 'senha_segura';
+CREATE USER 'rails_user'@'%' IDENTIFIED BY '05092004';
 GRANT ALL PRIVILEGES ON g_prod_development.* TO 'rails_user'@'%';
 FLUSH PRIVILEGES;
